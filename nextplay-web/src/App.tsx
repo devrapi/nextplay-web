be55494b51dfce259bb422372a9f99c0af97fa97
raffy <raffymaranan22@Gmail.com>
2026-07-20 10:16:27 +0800

    chore: initial frontend scaffold

---
+++ nextplay-web/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold text-primary underline text-center">NextPlay</h1>
+    </>
+  )
+}
+
+export default App
